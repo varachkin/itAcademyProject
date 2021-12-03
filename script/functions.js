f3757e7eda870e2b898f08d9dc41f54ac661e9e0
varachkin <varachkin@gmail.com>
2021-12-03 16:49:20 +0300

    feat: add function create input

--- script/functions.js
+++ script/functions.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const eye = document.querySelector('#eye');
-const eyeOff = document.querySelector('#eye-off');
 const password = document.querySelector('#password');
 
 function showEye() {
+    const eye = document.querySelector('.eye');
+    const eyeOff = document.querySelector('.eye-off');
     if (eye.style.display === 'none') {
         eyeOff.style.display = 'none';
         eye.style.display = 'inline';
@@ -14,4 +14,16 @@ function showEye() {
         eyeOff.style.display = 'inline';
         password.type = 'text';
     }
+}
+
+function createInputForm(x) {
+    const form = document.querySelector('.main-form');
+    const confPass = document.createElement('label');
+    const btnBlock = document.querySelector('.main-form-button-block');
+    confPass.innerHTML = `
+            <span>Confirm Password</span>
+            <input type="password" id="confirmPassword">
+            <span class="material-icons eye" id="eye" onclick="showEye()">visibility</span>
+            <span class="material-icons eye-off" id="eye-off" onclick="showEye()">visibility_off</span>`;
+    form.insertBefore(confPass, btnBlock);
 }
