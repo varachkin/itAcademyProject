5b30b989ec18fd1660eabd91f7d88a39b2fca7da
varachkin <varachkin@gmail.com>
2021-12-22 20:10:13 +0300

    fix: correct script.js, add header popup

--- script/functions.js
+++ script/functions.js
@@ -19,6 +19,7 @@ function showEye() {
 function createInputForm(x) {
     const form = document.querySelector('.main-form');
     const confPass = document.createElement('label');
+    confPass.className = 'input-form';
     const btnBlock = document.querySelector('.main-form-button-block');
     confPass.innerHTML = `
             <span>Confirm Password</span>
@@ -26,4 +27,36 @@ function createInputForm(x) {
             <span class="material-icons eye" id="eye" onclick="showEye()">visibility</span>
             <span class="material-icons eye-off" id="eye-off" onclick="showEye()">visibility_off</span>`;
     form.insertBefore(confPass, btnBlock);
+}
+
+function showHeader(item) {
+    const popup = document.querySelector('#popup-header');
+    const titlePopup = document.querySelector('.popup-header-title');
+    switch (item) {
+        case 'person':
+            titlePopup.innerText = 'Personal data';
+            const ulPopupHeader = document.createElement('ul');
+            ulPopupHeader.classList.add('popup-header-container-info');
+            for (let i = 0; i < 4; i++) {
+                let li = document.createElement('li');
+                li.classList.add('popup-li');
+                ulPopupHeader.append(li);
+            }
+            const arrUlPopup = document.querySelectorAll('.popup-li');
+            for (let el of arrUlPopup) {
+                el.append(document.createElement("span").innerText);
+            }
+            popup.append(ulPopupHeader);
+            break;
+        case 'basket':
+            titlePopup.innerText = 'Your basket';
+            break;
+    }
+
+    popup.style.transform = 'perspective(500px) translate(0, 0) rotateX(0)';
+}
+
+function closeHeader() {
+    const popup = document.querySelector('#popup-header');
+    popup.style.transform = 'perspective(200px) translate(0, -100%) rotateX(45deg)'
 }
--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
-'use strict'
+'use strict';
 
 let doughEl = document.getElementsByName("dough");
 let sizeEl = document.getElementsByName("size");
 let componentsEl = document.getElementsByName("components");
 let additionallyEl = document.getElementsByName("additionally");
-
+const popup = document.getElementById('popup');
+const popupContent = document.getElementById('popup_content');
 let num_components = 0;
 let sendObj = {};
 
@@ -22,39 +23,33 @@ let finalCostPizza;
 
 
 //   Функция заполнения объекта свойствами готовой пиццы
-function cooking(){
+function cooking() {
     costPizza = 0;
     caloriesPizza = 0;
     pizzaObj.DoughObj.cost = pizzaObj.DoughObj.calories = pizzaObj.SizeObj.cost = pizzaObj.SizeObj.calories = pizzaObj.ComponentsObj.cost = pizzaObj.ComponentsObj.calories = pizzaObj.AdditionallyObj.cost = pizzaObj.AdditionallyObj.calories = 0;
-    console.log(pizzaObj);
-    console.log(finalPizzaObj);
-    console.log(DoughObj);
-    console.log(SizeObj);
-    console.log(ComponentsObj);
-    console.log(AdditionallyObj);
 
-    if(doughEl){
-        for(let i = 0; i < doughEl.length; i++){
-            if(doughEl[i].checked){
-                for(let key in DoughObj){
-                    if(doughEl[i].id === key){
+    if (doughEl) {
+        for (let i = 0; i < doughEl.length; i++) {
+            if (doughEl[i].checked) {
+                for (let key in DoughObj) {
+                    if (doughEl[i].id === key) {
                         pizzaObj.DoughObj.cost = DoughObj[key]['cost'];
                         pizzaObj.DoughObj.calories = DoughObj[key]['calories'];
-                        if(sizeEl){
-                            for(let x = 0; x < sizeEl.length; x++){
-                                if(sizeEl[x].checked && doughEl[i].checked){
-                                    for(let key in SizeObj){
-                                        if(sizeEl[x].id === key){
+                        if (sizeEl) {
+                            for (let x = 0; x < sizeEl.length; x++) {
+                                if (sizeEl[x].checked && doughEl[i].checked) {
+                                    for (let key in SizeObj) {
+                                        if (sizeEl[x].id === key) {
                                             pizzaObj.SizeObj.cost = SizeObj[key]["cost"] * pizzaObj.DoughObj.cost;
                                             pizzaObj.SizeObj.calories = SizeObj[key]["calories"] * pizzaObj.DoughObj.calories;
                                         }
                                     }
-                                    if(componentsEl){
-                                        for(let i = 0; i < componentsEl.length; i++){
-                                            if(componentsEl[i].checked){
+                                    if (componentsEl) {
+                                        for (let i = 0; i < componentsEl.length; i++) {
+                                            if (componentsEl[i].checked) {
                                                 num_components++;
-                                                for(let key in ComponentsObj){
-                                                    if(componentsEl[i].id === key){
+                                                for (let key in ComponentsObj) {
+                                                    if (componentsEl[i].id === key) {
                                                         pizzaObj.ComponentsObj.cost += ComponentsObj[key]["cost"];
                                                         pizzaObj.ComponentsObj.calories += ComponentsObj[key]["calories"];
                                                     }
@@ -64,11 +59,11 @@ function cooking(){
                                     }
                                 }
                             }
-                            if(additionallyEl){
-                                for(let i = 0; i < additionallyEl.length; i++){
-                                    if(additionallyEl[i].checked){
-                                        for(let key in AdditionallyObj){
-                                            if(additionallyEl[i].id === key){
+                            if (additionallyEl) {
+                                for (let i = 0; i < additionallyEl.length; i++) {
+                                    if (additionallyEl[i].checked) {
+                                        for (let key in AdditionallyObj) {
+                                            if (additionallyEl[i].id === key) {
                                                 pizzaObj.AdditionallyObj.cost += AdditionallyObj[key]["cost"];
                                                 pizzaObj.AdditionallyObj.calories += AdditionallyObj[key]["calories"];
                                             }
@@ -94,25 +89,25 @@ function cooking(){
     cost(costPizza);
     console.log(num_components);
 
-    if(finalCostPizza !== 0){
+    if (finalCostPizza !== 0) {
         document.getElementById('information_cost').innerHTML = 'Cost: ' + costPizza.toFixed(2) + ' + ' + taxCost.toFixed(2) + ' = ' + ' $' + '<span>' + finalCostPizza.toFixed(2) + '</span>';
         document.getElementById('information_calories').innerHTML = 'Calories: ' + '<span>' + caloriesPizza + '</span>' + '  Kkal';
     }
-    if(pizzaObj.DoughObj['cost'] === 0){
+    if (pizzaObj.DoughObj['cost'] === 0) {
         document.getElementById('text_popup').innerHTML = 'No <span>dough</span> type selected. Select the type of pizza <span>dough</span> !!!';
         viewPopup();
         return;
     }
-    if(finalPizzaObj.SizeObj['cost'] === 0){
+    if (finalPizzaObj.SizeObj['cost'] === 0) {
         document.getElementById('text_popup').innerHTML = 'No dough <span>size</span> selected. Select the type of pizza <span>size</span> !!!';
         viewPopup();
         return;
     }
-    if(pizzaObj.ComponentsObj['cost'] === 0){
+    if (pizzaObj.ComponentsObj['cost'] === 0) {
         document.getElementById('text_popup').innerHTML = 'No <span>components</span> selected. Select the <span>components</span> of pizza !!!';
         viewPopup();
-    }else{
-        if(num_components < 3){
+    } else {
+        if (num_components < 3) {
             document.getElementById('text_popup').innerHTML = 'You added too few <span>components</span> to the pizza. <br>The minimum number of <span>components</span> is 3 !!!';
             viewPopup();
             num_components = 0;
@@ -136,25 +131,25 @@ function cooking(){
 }
 
 //   Функция отображения предупреждения
-function viewPopup(){
-    document.getElementById('popup').style.opacity = '1';
-    document.getElementById('popup').style.visibility = 'visible';
-    document.getElementById('popup_content').style.transform = 'perspective(600px) translate(0, 0) rotateX(0)';
+function viewPopup() {
+    popup.style.opacity = '1';
+    popup.style.visibility = 'visible';
+    popupContent.style.transform = 'perspective(600px) translate(0, 0) rotateX(0)';
 }
 
 //   Функция закрытия предупреждения
-function closePopup(){
-    document.getElementById('popup').style.opacity = '0';
-    document.getElementById('popup').style.visibility = 'hidden';
-    document.getElementById('popup_content').style.transform = 'perspective(600px) translate(0, -200%) rotateX(45deg)';
+function closePopup() {
+    popup.style.opacity = '0';
+    popup.style.visibility = 'hidden';
+    popupContent.style.transform = 'perspective(600px) translate(0, -200%) rotateX(45deg)';
 }
 
 //   Функция расчитывающая стоимость пиццы.
-function cost(num){
+function cost(num) {
     finalPizzaObj.structure = '';
     //   Запись информации о тесте в объект finalPizzaObj
     finalPizzaObj.DoughObj["cost"] = pizzaObj.DoughObj["cost"];
-    for(let x = 0; x < doughEl.length; x++) {
+    for (let x = 0; x < doughEl.length; x++) {
         if (doughEl[x].checked) {
             for (let key in DoughObj) {
                 if (doughEl[x].id === key) {
@@ -165,7 +160,7 @@ function cost(num){
     }
     //   Запись информации о размере в finalPizzaObj
     finalPizzaObj.SizeObj["cost"] = pizzaObj.SizeObj["cost"] * pizzaObj.DoughObj["cost"];
-    for(let x = 0; x < sizeEl.length; x++) {
+    for (let x = 0; x < sizeEl.length; x++) {
         if (sizeEl[x].checked) {
             for (let key in SizeObj) {
                 if (sizeEl[x].id === key) {
@@ -176,7 +171,7 @@ function cost(num){
     }
 
     //   Запись информации о добавленных ингредиентах в объект finalPizzaObj
-    for(let i = 0; i < componentsEl.length; i++) {
+    for (let i = 0; i < componentsEl.length; i++) {
         if (componentsEl[i].checked) {
             for (let key in ComponentsObj) {
                 if (componentsEl[i].id === key) {
@@ -188,7 +183,7 @@ function cost(num){
     finalPizzaObj.ComponentsObj['cost'] = pizzaObj.ComponentsObj['cost'];
 
     //   Запись информации о дополнительных ингредиентах в объект finalPizzaObj
-    for(let i = 0; i < additionallyEl.length; i++) {
+    for (let i = 0; i < additionallyEl.length; i++) {
         if (additionallyEl[i].checked) {
             for (let key in AdditionallyObj) {
                 if (additionallyEl[i].id === key) {
@@ -201,15 +196,15 @@ function cost(num){
     console.log(finalPizzaObj);
 
     //   Расчет конечной стоимости пиццы с учетом наценки
-    if(num < 100){
+    if (num < 100) {
         finalCostPizza = num + num * 0.2;
         taxCost = num * 0.2;
     }
-    if(num >= 20 && num <= 35){
+    if (num >= 20 && num <= 35) {
         finalCostPizza = num + num * 0.15;
         taxCost = num * 0.15;
     }
-    if(num > 35){
+    if (num > 35) {
         finalCostPizza = num + num * 0.1;
         taxCost = num * 0.1;
     }
@@ -218,57 +213,85 @@ function cost(num){
 }
 
 //   Функция отображения теста
-function viewImgDough(){
-    for(let i = 0; i < doughEl.length; i++){
-        if(doughEl[i].checked){
+function viewImgDough(x) {
+    // const blockDough = document.querySelector('.img_dough_block');
+    // blockDough.firstChild.remove();
+    // const img = blockDough.appendChild(document.createElement('img'));
+    // img.classList.add('img_dough');
+    // switch (x) {
+    //     case 1:
+    //         img.src = "img/dough/standard.png";
+    //         break;
+    //     case 2:
+    //         img.src = "img/dough/calzone.png";
+    //         break;
+    //     case 3:
+    //         img.src = "img/dough/calzone.png";
+    //         break;
+    //     case 4:
+    //         img.src = "img/dough/calzone.png";
+    //         break;
+    //     case 5:
+    //         img.src = "img/dough/calzone.png";
+    //         break;
+    // }
+    // img.style.transition = 1 + 's';
+    // img.style.opacity = '1';
+    for (let i = 0; i < doughEl.length; i++) {
+        if (doughEl[i].checked) {
             document.querySelectorAll('.img_dough_block img')[i].style.opacity = "1";
-        }else{
+        } else {
             document.querySelectorAll('.img_dough_block img')[i].style.opacity = "0";
         }
     }
 }
 
+document.querySelector('#st').addEventListener('click', () => {
+    viewImgDough(2)
+});
+
+
 //   Функция отображения размера пиццы
-function viewImgSize(num){
-    if(num === 32){
-        for(let i = 0; i < doughEl.length; i++){
-            document.querySelectorAll('.img_dough_block img')[i].style.width = "260px";
-            document.querySelectorAll('.img_dough_block img')[i].style.height = "260px";
-            }
-        for(let n = 0; n < componentsEl.length; n++){
-            document.querySelectorAll('.img_components_block img')[n].style.width = "227px";
-            document.querySelectorAll('.img_components_block img')[n].style.height = "227px";
+function viewImgSize(num) {
+    if (num === 32) {
+        for (let i = 0; i < doughEl.length; i++) {
+            document.querySelectorAll('.img_dough_block img')[i].style.width = "80%";
+            document.querySelectorAll('.img_dough_block img')[i].style.height = "80%";
+        }
+        for (let n = 0; n < componentsEl.length; n++) {
+            document.querySelectorAll('.img_components_block img')[n].style.width = "66%";
+            document.querySelectorAll('.img_components_block img')[n].style.height = "66%";
         }
     }
-    if(num === 40){
-        for(let i = 0; i < doughEl.length; i++){
-            document.querySelectorAll('.img_dough_block img')[i].style.width = "320px";
-            document.querySelectorAll('.img_dough_block img')[i].style.height = "320px";
+    if (num === 40) {
+        for (let i = 0; i < doughEl.length; i++) {
+            document.querySelectorAll('.img_dough_block img')[i].style.width = "95%";
+            document.querySelectorAll('.img_dough_block img')[i].style.height = "95%";
         }
-        for(let n = 0; n < componentsEl.length; n++){
-            document.querySelectorAll('.img_components_block img')[n].style.width = "280px";
-            document.querySelectorAll('.img_components_block img')[n].style.height = "280px";
+        for (let n = 0; n < componentsEl.length; n++) {
+            document.querySelectorAll('.img_components_block img')[n].style.width = "81%";
+            document.querySelectorAll('.img_components_block img')[n].style.height = "81%";
         }
     }
-    if(num === 45){
-        for(let i = 0; i < doughEl.length; i++){
-            document.querySelectorAll('.img_dough_block img')[i].style.width = "370px";
-            document.querySelectorAll('.img_dough_block img')[i].style.height = "370px";
+    if (num === 45) {
+        for (let i = 0; i < doughEl.length; i++) {
+            document.querySelectorAll('.img_dough_block img')[i].style.width = "114%";
+            document.querySelectorAll('.img_dough_block img')[i].style.height = "114%";
         }
-        for(let n = 0; n < componentsEl.length; n++){
-            document.querySelectorAll('.img_components_block img')[n].style.width = "320px";
-            document.querySelectorAll('.img_components_block img')[n].style.height = "320px";
+        for (let n = 0; n < componentsEl.length; n++) {
+            document.querySelectorAll('.img_components_block img')[n].style.width = "100%";
+            document.querySelectorAll('.img_components_block img')[n].style.height = "100%";
         }
     }
 }
 
 //   Функция отображения компонентов пиццы
-function viewImgComponents(){
-    for(let i = 0; i < componentsEl.length; i++){
-        if(componentsEl[i].checked){
+function viewImgComponents() {
+    for (let i = 0; i < componentsEl.length; i++) {
+        if (componentsEl[i].checked) {
             document.querySelectorAll('.img_components_block img')[i].style.opacity = "1";
-            for(let n = 0; n < doughEl.length; n++){
-                if(doughEl[n].checked){
+            for (let n = 0; n < doughEl.length; n++) {
+                if (doughEl[n].checked) {
                     componentsEl[i].checked = true;
                     document.querySelectorAll('.img_components_block img')[i].style.opacity = "1";
                     break;
@@ -276,35 +299,35 @@ function viewImgComponents(){
                 document.querySelectorAll('.img_components_block img')[i].style.opacity = "0";
                 componentsEl[i].checked = false;
             }
-        }else{
+        } else {
             document.querySelectorAll('.img_components_block img')[i].style.opacity = "0";
         }
     }
 }
 
 //   Функция отображения дополнительных ингридиентов
-function viewImgAdditionally(){
-    for(let i = 0; i < additionallyEl.length; i++){
-        if(additionallyEl[i].checked){
+function viewImgAdditionally() {
+    for (let i = 0; i < additionallyEl.length; i++) {
+        if (additionallyEl[i].checked) {
             document.querySelectorAll('.img_additionally_block img')[i].style.opacity = "1";
-        }else{
+        } else {
             document.querySelectorAll('.img_additionally_block img')[i].style.opacity = "0";
         }
     }
 }
 
 //   Функция сброса
-function reset(){
-    for(let i =0; i < doughEl.length; i++){
+function reset() {
+    for (let i = 0; i < doughEl.length; i++) {
         doughEl[i].checked = false;
     }
-    for(let i =0; i < sizeEl.length; i++){
+    for (let i = 0; i < sizeEl.length; i++) {
         sizeEl[i].checked = false;
     }
-    for(let i =0; i < componentsEl.length; i++){
+    for (let i = 0; i < componentsEl.length; i++) {
         componentsEl[i].checked = false;
     }
-    for(let i =0; i < additionallyEl.length; i++){
+    for (let i = 0; i < additionallyEl.length; i++) {
         additionallyEl[i].checked = false;
     }
 
